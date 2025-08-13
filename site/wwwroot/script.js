04066e80e39758a3894f624f5a56d528b4915e78
Fidel <fidel.garcia@grupo-bip.com>
2025-08-13 18:30:07 +0200

    pagina

--- site/wwwroot/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('showTimeBtn').addEventListener('click', function() {
-    const now = new Date();
-    document.getElementById('dateTimeDisplay').textContent = 'Fecha y Hora: ' + now.toLocaleString();
-});
